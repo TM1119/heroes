89fd3b454b7a6bfcce3f14e18ff8d38e83988db2
tm <13121039@qq.com>
2019-07-08 23:30:09 +0800

    路由组件----武器列表和装备列表

--- src/main.js
+++ src/main.js
@@ -9,12 +9,18 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 // 导入HeroList组件
 import HeroList from './views/hero/HeroList.vue'
+//导入WeaponList组件
+import WeaponList from './views/weapon/WeaponList.vue'
+//导入EquipList组件
+import EquipList from './views/equip/EquipList.vue'
 // 创建路由对象
 const router = new VueRouter({
   // 配置路由规则
   routes:[
       {path:"/", redirect:"/hero"},
-      {name:"hero", path:"/hero", component:HeroList}
+      {name:"hero", path:"/hero", component:HeroList},
+      {name:'weapon',path:'/weapon', component:WeaponList},
+      {name:'equip',path:'/equip', component:EquipList}
   ]
 })
 new Vue({
