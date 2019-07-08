e364d65c8a738d1263eda21be234657d660e9848
tm <13121039@qq.com>
2019-07-08 23:01:59 +0800

    实现英雄列表路由组件

--- src/main.js
+++ src/main.js
@@ -5,14 +5,16 @@ import '../node_modules/bootstrap-3.3.7-dist/css/bootstrap.css'
 import './assets/css/index.css'
 //导入vue-router
 import VueRouter from 'vue-router'
-注册VueRouter插件
+// 注册VueRouter插件
 Vue.use(VueRouter)
-
+// 导入HeroList组件
+import HeroList from './views/hero/HeroList.vue'
 // 创建路由对象
 const router = new VueRouter({
   // 配置路由规则
   routes:[
-
+      {path:"/", redirect:"/hero"},
+      {name:"hero", path:"/hero", component:HeroList}
   ]
 })
 new Vue({
