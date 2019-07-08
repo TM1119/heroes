f909a72b899a11a56c5b46cd87ea3bf0aac6f851
tm <13121039@qq.com>
2019-07-08 22:15:22 +0800

    添加公共部分

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入外部css
+import '../node_modules/bootstrap-3.3.7-dist/css/bootstrap.css'
+import './assets/css/index.css'
+// 加载路由模块
 
-Vue.config.productionTip = false
 
 new Vue({
+  el:'#app',
   render: h => h(App),
 }).$mount('#app')
