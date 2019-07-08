f3bcc8303f562b81e3a53fd5d365deae92263217
tm <13121039@qq.com>
2019-07-08 22:35:43 +0800

    配置路由

--- src/main.js
+++ src/main.js
@@ -3,10 +3,19 @@ import App from './App.vue'
 // 引入外部css
 import '../node_modules/bootstrap-3.3.7-dist/css/bootstrap.css'
 import './assets/css/index.css'
-// 加载路由模块
+//导入vue-router
+import VueRouter from 'vue-router'
+注册VueRouter插件
+Vue.use(VueRouter)
 
+// 创建路由对象
+const router = new VueRouter({
+  // 配置路由规则
+  routes:[
 
+  ]
+})
 new Vue({
-  el:'#app',
+  router,
   render: h => h(App),
 }).$mount('#app')
