546f0f12d3a6e96356f88a54c6863f03eafa6953
tm <13121039@qq.com>
2019-07-09 09:56:09 +0800

    更改高亮显示用的linkActiveClass

--- src/router.js
+++ src/router.js
@@ -16,7 +16,8 @@ import HeroAdd  from './views/hero/HeroAdd.vue'
 const router = new VueRouter({
   // 选项
   // 当浏览器地址中的路由地址和router-link生成的a标签的href精确匹配的时候 添加类样式
-  linkExactActiveClass :'active',
+  // linkExactActiveClass :'active',
+  linkActiveClass :'active',
   // 配置路由规则
   routes:[
       {path:"/", redirect:"/hero"},
